ce4e24c9873b1cdd090365c574c9df35a2de229c
samuelamissah <skamissah@ug.edu.gh>
2024-02-17 11:07:40 +0000

    hdh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import Topbar from "./scenes/dashboard/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+
 
 function App() {
+  const { colorMode, theme } = useMode();
   return (
-    <div className="app">
-    
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+        <Topbar />
+          </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
